ecd40845c8b431734ec2ed5699302bdabf9b1f98
Marco Tulio Terra <marcotulio.mtt@gmail.com>
2022-07-19 09:54:21 -0300

    graph + binary tree

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
